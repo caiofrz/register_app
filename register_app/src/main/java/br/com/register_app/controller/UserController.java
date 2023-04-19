a092a907a54f50db212e1fc5c3c61869f86aa368
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-04-18 21:07:47 -0300

    FEAT: criando classes e requisições iniciais

---
+++ register_app/src/main/java/br/com/register_app/controller/UserController.java
@@ -0,0 +1,32 @@
+package br.com.register_app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.register_app.model.User;
+import br.com.register_app.model.UserRepository;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping(value = "/users")
+    public List<User> getUsers(){
+        return (List<User>) repository.findAll();
+    }
+
+
+    @PostMapping(value = "/users")
+    public User createUser(@RequestBody User user) {
+        return repository.save(user);
+    }
+
+
+}
