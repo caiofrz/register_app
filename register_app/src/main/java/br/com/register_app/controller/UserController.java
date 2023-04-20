d2c53d82821d5f096d7538e9362bc2d6889a45ad
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-04-20 15:28:02 -0300

    FEAT: add anotation para receber requisições da própria máquina

--- register_app/src/main/java/br/com/register_app/controller/UserController.java
+++ register_app/src/main/java/br/com/register_app/controller/UserController.java
@@ -3,6 +3,7 @@ package br.com.register_app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import br.com.register_app.model.User;
 import br.com.register_app.repository.UserRepository;
 
 @RestController
+@CrossOrigin("*") //Liberar entradas da mesma máquina
 public class UserController {
 
     @Autowired
